b831dfd169107d6df4d6049750f54230f0015993
bbcbear <bbcbear@yandex.ru>
2020-10-21 23:14:29 +0300

    Methods sheme.

--- cmd/football/football.go
+++ cmd/football/football.go
@@ -5,3 +5,31 @@ import "fmt"
 func main() {
     fmt.Println("hello world")
 }
+
+func GetConfig(){
+    
+}
+
+//Нужно будет потом раскидать функции на разные модули
+
+func AddNewPlayer(){
+
+}
+
+func RemovePlayer(){
+
+}
+
+func GetPlayerList(){
+
+}
+
+
+
+func GetNewLineUp(){
+
+}
+
+func GetManiskaWasher(){
+
+}
