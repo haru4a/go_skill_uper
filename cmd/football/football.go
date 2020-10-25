e9323a262f9d867c2a28d5bf7c1f95cf6030737f
bbcbear <bbcbear@yandex.ru>
2020-10-25 13:08:47 +0300

    Add worked mux settings

--- cmd/football/football.go
+++ cmd/football/football.go
@@ -1,51 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/player", getPlayerList).Methods("GET")
-    r.HandleFunc("/player", addNewPlayer).Methods("POST")
-    r.HandleFunc("/player", removePlayer).Methods("DELETE")
+func getConfig() {
 
-    r.HandleFunc("/newgame", getNewLineUp).Methods("GET")
-    r.HandleFunc("/endgame", getManiskaWasher).Methods("GET")
-    
-    fmt.Print("runned")
-
-
-
-}
-
-func getConfig(){
-    
 }
 
 //Нужно будет потом раскидать функции на разные модули
 
-func getPlayerList(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Test Player route")
-    return
-}
-func addNewPlayer(w http.ResponseWriter, r *http.Request){
-    return
+func getPlayerList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test Player route")
 }
 
-func removePlayer(w http.ResponseWriter, r *http.Request){
-    return
+func addNewPlayer(w http.ResponseWriter, r *http.Request) {
+
 }
 
+func removePlayer(w http.ResponseWriter, r *http.Request) {
 
-
-func getNewLineUp(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Test NewLineUp route")
-    return
 }
 
-func getManiskaWasher(w http.ResponseWriter, r *http.Request){
-    return
+func getNewLineUp(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test NewLineUp route")
+}
+
+func getManiskaWasher(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/player", getPlayerList).Methods("GET")
+	r.HandleFunc("/player", addNewPlayer).Methods("POST")
+	r.HandleFunc("/player", removePlayer).Methods("DELETE")
+
+	r.HandleFunc("/newgame", getNewLineUp).Methods("GET")
+	r.HandleFunc("/endgame", getManiskaWasher).Methods("GET")
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+
 }
